254d053e50ab1589da8889f4932f9f651879f6f9
waiwaaa <kx1115@tom.com>
2018-08-20 10:31:55 +0800

    增加选择值的倍数 指定最小刻度表示的值大小

--- app/src/main/java/com/yy/scalewineview/WineView.java
+++ app/src/main/java/com/yy/scalewineview/WineView.java
@@ -1,6 +1,5 @@
 package com.yy.scalewineview;
 
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -21,10 +20,17 @@ public class WineView extends View {
     private static final int ITEM_MIN_HEIGHT = 20;
 
     private float mDensity;
-    private int mMinValue=0, mValue = 50, mMaxValue = 100;
+    //最小值，当前值，最大值
+    private int mMinValue=0, mValue = 50, mMaxValue = 500;
+    // 选择的值为此数的倍数
+    private int mValueUnit=50;
+    //每个小刻度表示的value大小
+    private int mMinUnit = 10;
 
-    private int mGap = 4;// 最小刻度的像素宽度
-    private int mUnit=10;// 长刻度相对于最小刻度的倍数
+    // 最小刻度的像素宽度
+    private int mGap = 8;
+    // 长刻度相对于最小刻度的倍数
+    private int mUnit = 5;
 
     private int mLastX, mMove;
     private int mWidth, mHeight;
@@ -34,8 +40,12 @@ public class WineView extends View {
     private VelocityTracker mVelocityTracker;
     private OnValueChangeListener mListener;
 
+    float distance;
+
     public WineView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+
 
         mScroller = new Scroller(getContext());
         mDensity = getContext().getResources().getDisplayMetrics().density;
@@ -79,6 +89,8 @@ public class WineView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        distance=mGap * mDensity /mMinUnit;//value转成像素长度参数
+
         // 画笔
         Paint paint = new Paint();
         paint.setColor(Color.GRAY);
@@ -106,14 +118,18 @@ public class WineView extends View {
     private void drawScaleLine(Canvas canvas, Paint linePaint) {
         canvas.save();
 
+        int maxUnit = mMinUnit*mUnit;
+
         //mValue
-        int mFixValue=mValue/mUnit*mUnit;
+        int mFixValue=mValue/maxUnit*maxUnit;
         int left=mFixValue-mMinValue;
         int right=mMaxValue-mFixValue;
 
-        int fixVuleX=mWidth/2 - Math.round((mValue-mFixValue) * mGap * mDensity);
-        int startX= Math.max(0,fixVuleX- Math.round(left* mGap * mDensity));
-        int endX= Math.min(fixVuleX+ Math.round(right* mGap * mDensity),mWidth);
+
+
+        int fixVuleX=mWidth/2 - Math.round((mValue-mFixValue) * distance);
+        int startX= Math.max(0,fixVuleX- Math.round(left* distance));
+        int endX= Math.min(fixVuleX+ Math.round(right* distance),mWidth);
         //画横线
         canvas.drawLine(startX,0,endX,0,linePaint);
 
@@ -123,12 +139,12 @@ public class WineView extends View {
         int width = mWidth, drawCount = 0;
         float xPosition = 0, textWidth = Layout.getDesiredWidth("0", textPaint);
 
-        for(int i=mMinValue;i<=mMaxValue;i++){
-            xPosition=fixVuleX+ Math.round((i-mFixValue) * mGap * mDensity);
+        for(int i=mMinValue;i<=mMaxValue;i+=mMinUnit){
+            xPosition=fixVuleX+ Math.round((i-mFixValue) * distance);
             if(xPosition<-100)continue;
             if(xPosition>width+100)return;
 
-            if(i%mUnit==0){
+            if(i%maxUnit==0){
                 canvas.drawLine(xPosition, getPaddingTop(), xPosition, mDensity * ITEM_MAX_HEIGHT, linePaint);
                 int numSize= String.valueOf(i).length();
                 canvas.drawText(String.valueOf(i), xPosition - (textWidth * numSize / 2), getHeight() - textWidth, textPaint);
@@ -209,16 +225,18 @@ public class WineView extends View {
         mVelocityTracker.computeCurrentVelocity(1000);
         float xVelocity = mVelocityTracker.getXVelocity();
         if (Math.abs(xVelocity) > mMinVelocity) {
-            mScroller.fling(0, 0, (int) xVelocity, 0, mMinValue, mMaxValue, 0, 0);
+            //mScroller.fling(0, 0, (int) xVelocity, 0, mMinValue, mMaxValue, 0, 0);
+            mScroller.fling(0, 0, (int) xVelocity, 0, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, 0);
         }
     }
 
     private void changeMoveAndValue() {
-        int tValue = (int) (mMove / (mGap * mDensity));
+        int tValue = (int) (mMove / (distance));
         if (Math.abs(tValue) > 0) {
             mValue += tValue;
-            mMove -= tValue * mGap * mDensity;
+            mMove -= tValue * distance;
             if (mValue <= 0 || mValue > mMaxValue) {
+                //mValue = Math.round(1.0f*mValue/mValueUnit)*mValueUnit;
                 mValue = mValue <= 0 ? 0 : mMaxValue;
                 mMove = 0;
                 mScroller.forceFinished(true);
@@ -229,8 +247,11 @@ public class WineView extends View {
     }
 
     private void countMoveEnd() {
-        int roundMove = Math.round(mMove / (mGap * mDensity));
+        int roundMove = Math.round(mMove / (distance));
         mValue = mValue + roundMove;
+
+        mValue = Math.round(1.0f*mValue/mValueUnit)*mValueUnit;
+
         mValue = mValue <= 0 ? 0 : mValue;
         mValue = mValue > mMaxValue ? mMaxValue : mValue;
 
